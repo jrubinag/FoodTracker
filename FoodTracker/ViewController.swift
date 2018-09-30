26c9625bca85c79f61511d7cbbaecac807151ddb
Jonathan Rubina <jonathanrubina@Jonathans-MacBook-Pro.local>
2018-09-30 10:26:05 -0500

    Deleting mealname label

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -12,7 +12,6 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
 
     //MARK: Properties
     @IBOutlet weak var nameTextField: UITextField!
-    @IBOutlet weak var mealNameLabel: UILabel!
     @IBOutlet weak var photoImageView: UIImageView!
     @IBOutlet weak var ratingControl: RatingControl!
     
@@ -38,7 +37,6 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
     }
     
     func textFieldDidEndEditing(_ textField: UITextField) {
-        mealNameLabel.text = nameTextField.text
     }
     
     //MARK: UIImagePickerControllerDelegate
